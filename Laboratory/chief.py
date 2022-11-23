b5dbc7c254cfc272bb392dbb7cc51acaf9d48ca4
Tarmica <lilskyforever@gmail.com>
2022-11-23 19:38:23 +0200

    updat again

---
+++ Laboratory/chief.py
@@ -0,0 +1,23 @@
+import socket
+import select
+
+
+ADDR =(socket.gethostbyname(socket.gethostname), 6969)
+
+socketslist = []
+
+
+def chatserver():
+    chief_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    chief_socket.bind(ADDR)
+
+    socketslist.append(chief_socket)
+
+    chief_socket.listen(10)
+
+
+    while 1:
+        ready_to_read, ready_to_write, in_error = select.select(socketslist, [], [], 0)
+        
+    for sock in ready_to_read:
+        if sock == chief_socket:
